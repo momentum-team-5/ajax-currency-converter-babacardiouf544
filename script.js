3c1a31f3a2bd538df1582606faabd24997d31202
babacardiouf544 <babacardi@gmail.com>
2020-09-24 09:26:30 -0400

    couldn't figure out how to display result

--- script.js
+++ script.js
@@ -34,3 +34,20 @@ const currencies = [
   'MYR'
 ]
 
+const userUrl = 'https://api.exchangeratesapi.io/latest?base='
+
+
+//get website
+fetch(userUrl)
+.then(response => response.json())
+.then(function (data) {
+  console.log(data)
+})
+
+
+//let's insert the currrency option in the selector
+const currency = document.getElementById('base-currency')
+Object.keys(currencies).map(key => currency.add(new Option(currencies[key], key)))
+
+const currency1 = document.getElementById('base-currency1')
+Object.keys(currencies).map(key => currency1.add(new Option(currencies[key], key)))
